5c1e73581fce94aa1043e07c94793562add5e36c
SamIsTheFBI <mrqube001@gmail.com>
2023-11-13 20:04:34 +0530

    fixed build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,11 @@ export default defineConfig({
       "cdn.donmai.us",
     ],
   },
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
   markdown: {
     shikiConfig: {
       theme: 'rose-pine',
