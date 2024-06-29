c7bbd3f4bc4e760da975106c059cb20255e7b44a
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-06-29 20:03:19 +0530

    feat: add tech stack option to project card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), icon()],
   vite: {
     ssr: {
-      noExternal: ["react-icons"],
-    },
+      noExternal: ["react-icons"]
+    }
   },
   server: {
-    host: true,
-  },
+    host: true
+  }
 });
