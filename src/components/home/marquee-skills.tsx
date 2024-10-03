ff8399b4894343ec9e9837617a020edc33a6d8e6
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-10-04 00:19:49 +0530

    view transitions, selection color and anime page init

--- src/components/home/marquee-skills.tsx
+++ src/components/home/marquee-skills.tsx
@@ -1,40 +1,63 @@
 import Marquee from "@/components/magicui/marquee";
+import Arch from "@/assets/skills/arch.svg"
+import Astro from "@/assets/skills/astro.svg"
+import Aws from "@/assets/skills/aws.svg"
+import Bash from "@/assets/skills/bash.svg"
+import Bootstrap from "@/assets/skills/bootstrap.svg"
+import Bun from "@/assets/skills/bun.svg"
+import Clang from "@/assets/skills/c.svg"
+import Codepen from "@/assets/skills/codepen.svg"
+import Cpp from "@/assets/skills/cpp.svg"
+import Debian from "@/assets/skills/debian.svg"
+import Docker from "@/assets/skills/docker.svg"
+import Express from "@/assets/skills/express.svg"
+import Figma from "@/assets/skills/figma.svg"
+import Git from "@/assets/skills/git.svg"
+import Github from "@/assets/skills/github.svg"
+import Html from "@/assets/skills/html.svg"
+import Js from "@/assets/skills/js.svg"
+import Linux from "@/assets/skills/linux.svg"
+import Neovim from "@/assets/skills/neovim.svg"
+import Nextjs from "@/assets/skills/nextjs.svg"
+import Nodejs from "@/assets/skills/nodejs.svg"
+import Postgres from "@/assets/skills/postgres.svg"
+import Prisma from "@/assets/skills/prisma.svg"
+import React from "@/assets/skills/react.svg"
+import Supabase from "@/assets/skills/supabase.svg"
+import Tailwind from "@/assets/skills/tailwind.svg"
+import Ts from "@/assets/skills/ts.svg"
+import Vite from "@/assets/skills/vite.svg"
 
 const skills = [
-  { link: "https://skillicons.dev/icons?i=arch" },
-  { link: "https://skillicons.dev/icons?i=astro" },
-  { link: "https://skillicons.dev/icons?i=aws" },
-  { link: "https://skillicons.dev/icons?i=bash" },
-  { link: "https://skillicons.dev/icons?i=bun" },
-  { link: "https://skillicons.dev/icons?i=react" },
-  { link: "https://skillicons.dev/icons?i=nextjs" },
-  { link: "https://skillicons.dev/icons?i=nodejs" },
-  { link: "https://skillicons.dev/icons?i=html" },
-  { link: "https://skillicons.dev/icons?i=js" },
-  { link: "https://skillicons.dev/icons?i=ts" },
-  { link: "https://skillicons.dev/icons?i=css" },
-  { link: "https://skillicons.dev/icons?i=tailwind" },
-  { link: "https://skillicons.dev/icons?i=bootstrap" },
-  { link: "https://skillicons.dev/icons?i=c" },
-  { link: "https://skillicons.dev/icons?i=cpp" },
-  { link: "https://skillicons.dev/icons?i=codepen" },
-  { link: "https://skillicons.dev/icons?i=debian" },
-  { link: "https://skillicons.dev/icons?i=docker" },
-  { link: "https://skillicons.dev/icons?i=express" },
-  { link: "https://skillicons.dev/icons?i=figma" },
-  { link: "https://skillicons.dev/icons?i=firebase" },
-  { link: "https://skillicons.dev/icons?i=git" },
-  { link: "https://skillicons.dev/icons?i=github" },
-  { link: "https://skillicons.dev/icons?i=go" },
-  { link: "https://skillicons.dev/icons?i=linux" },
-  { link: "https://skillicons.dev/icons?i=mongodb" },
-  { link: "https://skillicons.dev/icons?i=neovim" },
-  { link: "https://skillicons.dev/icons?i=postgres" },
-  { link: "https://skillicons.dev/icons?i=prisma" },
-  { link: "https://skillicons.dev/icons?i=py" },
-  { link: "https://skillicons.dev/icons?i=redux" },
-  { link: "https://skillicons.dev/icons?i=supabase" },
-  { link: "https://skillicons.dev/icons?i=vite" }
+  Arch,
+  Astro,
+  Bash,
+  Aws,
+  Bash,
+  Bootstrap,
+  Bun,
+  Clang,
+  Codepen,
+  Cpp,
+  Debian,
+  Docker,
+  Express,
+  Figma,
+  Git,
+  Github,
+  Html,
+  Js,
+  Linux,
+  Neovim,
+  Nextjs,
+  Nodejs,
+  Postgres,
+  Prisma,
+  React,
+  Supabase,
+  Tailwind,
+  Ts,
+  Vite
 ];
 
 const firstRow = skills.slice(0, skills.length / 2);
@@ -44,13 +67,13 @@ export function MarqueeSkills() {
   return (
     <div className="relative h-40 flex flex-col items-center justify-center overflow-hidden rounded-lg bg-background space-y-3 max-w-xs sm:max-w-2xl">
       <Marquee className="[--duration:30s] max-w-xs sm:max-w-2xl">
-        {firstRow.map(icon => (
-          <img key={icon.link} src={icon.link} decoding="async" alt="skill" />
+        {firstRow.map((icon, idx) => (
+          <img key={idx} src={icon.src} decoding="async" alt="skill" />
         ))}
       </Marquee>
       <Marquee reverse className="[--duration:30s] max-w-xs sm:max-w-2xl">
-        {secondRow.map(icon => (
-          <img key={icon.link} src={icon.link} decoding="async" alt="skill" />
+        {secondRow.map((icon, idx) => (
+          <img key={idx} src={icon.src} decoding="async" alt="skill" />
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background"></div>
