a1344f279087cadbe6f437410702144cc1f8c44d
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2025-02-07 20:14:05 +0530

    update workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://samisthefbi.github.io",
   integrations: [react(), tailwind({
     applyBaseStyles: false
   }), icon()],
